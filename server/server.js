2ce591ba0ffe50334f05f9e0e4dc6f7ac14a8227
Manpreet singh <manpreetsingh57736@gmail.com>
2025-08-03 12:10:46 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin:process.env.cross_origin,
+  origin:process.env.cross_origin||"http://localhost:4000/",
   credentials: true,
 }));
 connectToDB();
