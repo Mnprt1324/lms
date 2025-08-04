808a775abf6802755d5be528c9de2c13c2bf6d2c
Manpreet singh <manpreetsingh57736@gmail.com>
2025-08-04 09:41:03 +0530

    multer.js file update

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,4 +1,6 @@
-const multer  = require('multer');
+const multer = require("multer")
 
-const upload = multer({ dest: 'uploads/' })
-module.exports =upload;
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+module.exports = upload;
