fc79e47b32caec72a4668192487d8894d67c8601
Mnprt1324 <160628185+Mnprt1324@users.noreply.github.com>
2025-08-04 10:13:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin:process.env.cross_origin||"http://localhost:4000/",
+  origin:'https://lms-5531.vercel.app',
   credentials: true,
 }));
 connectToDB();
